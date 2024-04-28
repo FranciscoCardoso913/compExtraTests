03e3386c2ffa371f753940cd878c6c73a0791f62
Félix Martins <felix.marcial@hotmail.com>
2024-04-28 15:50:23 +0100

    Added some more complex tests for Constant opt.

--- ConstantOptimizationTest.java
+++ ConstantOptimizationTest.java
@@ -12,7 +12,9 @@ import static org.junit.Assert.assertEquals;
 import static pt.up.fe.comp.TestUtils.getJmmOptimization;
 
 public class ConstantOptimizationTest {
-    // Currently, you will need to check the output on the ollir code
+    // Currently, you can easily modify the tests and check either the ollir code or the AST in the terminal
+    // Since some tests have while's and if's, and ollir may not handle them yet, you can use the AST to check the optimizations
+
     // Modify the tests if you want automatic testing
 
     @Test
@@ -29,8 +31,8 @@ public class ConstantOptimizationTest {
         semanticsResult = optimization.optimize(semanticsResult);
         System.out.println(semanticsResult.getRootNode().toTree());
          */
-
     }
+
     @Test
     public void testConstants() {
         var code = SpecsIo.getResource("pt/up/fe/comp/compExtraTests/apps/constant_optimization/Constants.jmm");
@@ -47,6 +49,72 @@ public class ConstantOptimizationTest {
         semanticsResult = optimization.optimize(semanticsResult);
         System.out.println(semanticsResult.getRootNode().toTree());
          */
+    }
+    @Test
+    public void testConstantsMultipleMethods() {
+        var code = SpecsIo.getResource("pt/up/fe/comp/compExtraTests/apps/constant_optimization/ConstantsMultipleMethods.jmm");
+        OllirResult optimizationResult = TestUtils.optimize(code, Collections.emptyMap());
+        System.out.println(optimizationResult.getOllirCode());
+
+        // to use only the AST optimizations, you need to use this (output will be significantly more verbose)
+        /*
+        JmmOptimization optimization = getJmmOptimization();
+        var parserResult = TestUtils.parse(code);
+        var semanticsResult = TestUtils.analyse(parserResult);
+        semanticsResult = optimization.optimize(semanticsResult);
+        System.out.println(semanticsResult.getRootNode().toTree());
+         */
+
+    }
+    @Test
+    public void testConstantsTricky() {
+        var code = SpecsIo.getResource("pt/up/fe/comp/compExtraTests/apps/constant_optimization/ConstantsTricky.jmm");
+
+        // this runs optimizations in both the AST and Ollir
+        // OllirResult optimizationResult = TestUtils.optimize(code, Collections.emptyMap());
+        // System.out.println(optimizationResult.getOllirCode());
+
+        // to use only the AST optimizations, you need to use this (output will be significantly more verbose)
+
+        JmmOptimization optimization = getJmmOptimization();
+        var parserResult = TestUtils.parse(code);
+        var semanticsResult = TestUtils.analyse(parserResult);
+        semanticsResult = optimization.optimize(semanticsResult);
+        System.out.println(semanticsResult.getRootNode().toTree());
+    }
+
+    @Test
+    public void testConstantsWithinLoop() {
+        var code = SpecsIo.getResource("pt/up/fe/comp/compExtraTests/apps/constant_optimization/ConstantsWithinLoop.jmm");
+
+        // this runs optimizations in both the AST and Ollir
+        // OllirResult optimizationResult = TestUtils.optimize(code, Collections.emptyMap());
+        // System.out.println(optimizationResult.getOllirCode());
+
+        // to use only the AST optimizations, you need to use this (output will be significantly more verbose)
+
+        JmmOptimization optimization = getJmmOptimization();
+        var parserResult = TestUtils.parse(code);
+        var semanticsResult = TestUtils.analyse(parserResult);
+        semanticsResult = optimization.optimize(semanticsResult);
+        System.out.println(semanticsResult.getRootNode().toTree());
+    }
+
+    @Test
+    public void testConstantsComplex() {
+        var code = SpecsIo.getResource("pt/up/fe/comp/compExtraTests/apps/constant_optimization/ConstantsComplex.jmm");
+
+        // this runs optimizations in both the AST and Ollir
+        // OllirResult optimizationResult = TestUtils.optimize(code, Collections.emptyMap());
+        // System.out.println(optimizationResult.getOllirCode());
+
+        // to use only the AST optimizations, you need to use this (output will be significantly more verbose)
+
+        JmmOptimization optimization = getJmmOptimization();
+        var parserResult = TestUtils.parse(code);
+        var semanticsResult = TestUtils.analyse(parserResult);
+        semanticsResult = optimization.optimize(semanticsResult);
+        System.out.println(semanticsResult.getRootNode().toTree());
     }
 
 }
